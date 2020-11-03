a223963fc53c08d46506f861da753716eb9148e1
michalke-it <67504436+michalke-it@users.noreply.github.com>
2020-11-03 09:36:43 +0100

    Create archtester.sh

---
+++ archtester.sh
@@ -0,0 +1,12 @@
+ARCH=$(uname -m)
+case "${ARCH}" in
+    x86_64*)
+        machine=amd64
+        RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w"
+    ;;
+    aarch64*)
+        machine=arm64
+        RUN CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w"
+    ;;
+    *)          exit $ARCH
+esac
